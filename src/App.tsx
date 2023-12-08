b4c6ac48e8912945e8f6f36b0edc308b7c32c0e4
Parker <parkermdinsmore@gmail.com>
2023-12-08 17:14:10 -0500

    finish basic scratch navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+import ButtonPage from "./pages/ButtonPage";
+import Sidebar from "./components/Sidebar";
+
 const App = () => {
   return (
-    <div>
-      Hello World
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+
+      <div className="col-span-5">
+        <Route pathTo="/">
+          <DropdownPage />
+        </Route>
+        <Route pathTo="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route pathTo="/buttons">
+          <ButtonPage />
+        </Route>
+      </div>
     </div>
   );
 };
--- src/context/navigation.tsx
+++ src/context/navigation.tsx
@@ -1,20 +1,42 @@
-import { createContext, useState } from "react";
-
-const NavigationContext = createContext({});
+import { createContext, useState, useEffect } from "react";
 
 interface NavigationProps {
   children: any;
 }
 
+interface NavigationContextType {
+  currentPath: string;
+  navigate: (toPath: string) => void;
+}
+
+const NavigationContext = createContext<NavigationContextType>({
+  currentPath: "/",
+  navigate: () => undefined, 
+});
+
 const NavigationProvider = ({ children }: NavigationProps) => {
   // default value for when our app first starts up (whatever path the user navigted to)
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
-  console.log("currentPath", currentPath);
+  useEffect(() => {
+    // fn that handles the user navigating forward / back in the browswer
+    // we are setting state to force a re-render when the user navigates forward / back
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", handler);
+
+    return () => window.removeEventListener("popstate", handler);
+  }, []);
+
+  const navigate = (pathTo: string) => {
+    window.history.pushState({}, "", pathTo);
+    setCurrentPath(pathTo);
+  };
 
   return (
-    <NavigationContext.Provider value={{}}>
-      {currentPath}
+    <NavigationContext.Provider value={{ currentPath, navigate }}>
       {children}
     </NavigationContext.Provider>
   );
