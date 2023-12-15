6967b57298e88089841a8c4f75ffa7ae82dce507
Parker <parkermdinsmore@gmail.com>
2023-12-15 16:02:39 -0500

    reusable table with optional sorting

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,40 +1,42 @@
-interface DataItem {
-  name: string;
-  color: string;
-  score: number;
-}
+import { Fragment } from "react";
+import { TableDataItem, TableConfigItem } from "../types/types";
 
 interface TableProps {
-  data: DataItem[];
+  data: TableDataItem[];
+  config: TableConfigItem[];
+  keyFn: (item: TableDataItem) => string; // make dev using Table component provide fn to create keys so data remains as reusable as possible
 }
 
-const Table = ({ data }: TableProps) => {
-  const tableRows = () => {
-    return data.map((item) => {
-      return (
-        <tr key={item.name} className="border-b">
-          <td className="p-3">{item.name}</td>
-          <td className="p-3">
-            <div className={`p-3 m-2 ${item.color}`}></div>
-          </td>
-          <td className="p-3">{item.score}</td>
-        </tr>
-      );
-    });
-  };
+const Table = ({ data, config, keyFn }: TableProps) => {
+  const renderedHeaders = config.map((column) => {
+    if (column.header) {
+      // allow returning table header cell without wrapping in a div (causes invalid HTML)
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
+    }
+
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((rowData) => {
+    return (
+      <tr key={keyFn(rowData)} className="border-b">
+        {config.map((column) => {
+          return (
+            <td key={column.label} className="p-3">
+              {column.render(rowData)}
+            </td>
+          );
+        })}
+      </tr>
+    );
+  });
 
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr className="border-b-2">
-          <th>Fruit</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
-      <tbody>
-        {tableRows()}
-      </tbody>
+      <tbody>{renderedRows}</tbody>
     </table>
   );
 };
--- src/pages/TablePage.tsx
+++ src/pages/TablePage.tsx
@@ -1,17 +1,40 @@
 import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
+import { TableDataItem, TableConfigItem } from "../types/types";
 
 const TablePage = () => {
-  const data = [
-    { name: "apple", color: "bg-red-500", score: 1 },
+  const data: TableDataItem[] = [
+    { name: "red", color: "bg-red-500", score: 1 },
     { name: "orange", color: "bg-orange-500", score: 2 },
-    { name: "banana", color: "bg-yellow-500", score: 3 },
-    { name: "lime", color: "bg-green-500", score: 4 },
-    { name: "blue raspberry", color: "bg-blue-500", score: 5 },
-    { name: "purple cow", color: "bg-purple-500", score: 6 },
-  ]
+    { name: "yellow", color: "bg-yellow-500", score: 3 },
+    { name: "green", color: "bg-green-500", score: 69 },
+    { name: "blue", color: "bg-blue-500", score: 5 },
+    { name: "purple", color: "bg-purple-500", score: 6 },
+  ];
+
+  const config: TableConfigItem[] = [
+    {
+      label: "Name",
+      render: (item) => item.name,
+      sortValue: (item) => item.name,
+    },
+    {
+      label: "Color",
+      render: (item) => <div className={`p-3 m-2 ${item.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (item) => item.score,
+      sortValue: (item) => item.score,
+    },
+  ];
+
+  const keyFn = (item: TableDataItem) => item.name;
+
   return (
-    <div>
-      <Table data={data} />
+    <div className="flex justify-evenly">
+      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 };
